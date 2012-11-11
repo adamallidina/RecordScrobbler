03c6b8f82d6a754ac2f083001959af31b876e8e9
Adam Allidina <adam.allidina@gmail.com>
2012-11-10 22:43:09 -0500

    Implemented hashing function, returns the signature for last.fm api calls

--- lastfm.py
+++ lastfm.py
@@ -5,7 +5,8 @@
 #
 ###############################################################################
 
-import requests, webbrowser
+import requests, webbrowser, hashlib
+from ConfigParser import SafeConfigParser
 
 class session(object):
 
@@ -18,6 +19,7 @@ class session(object):
     self.apiKey    = "021ab74b3f5405138a23c42b8ff1c746"
     self.predicate = "http://ws.audioscrobbler.com/2.0/?method=auth.gettoken"
     self.token     = self.gettoken()
+    self.signature = self.getsignature()
 
   def gettoken(self):
     url       = self.predicate + "&api_key=" + self.apiKey + "&format=json"
@@ -26,13 +28,19 @@ class session(object):
     token     = json['token']
     return token
 
+  def getsignature(self):
+    # Get secret key from config file
+    parser  = SafeConfigParser()
+    parser.read('config.txt')
+    secret  = parser.get('secret', 'key')
+    # The string that needs to be hashed, as specified by last.fm docs
+    tohash  = "api_key" + self.apiKey + "methodauth.getSessiontoken" + secret
+    hashobj = hashlib.md5(tohash)
+    return hashobj.hexdigest()
+
   def auth(self):
     predicate = "http://www.last.fm/api/auth/?api_key="
     # Open up users default web browser to last.fm authentication page
     webbrowser.open(predicate + self.apiKey + "&token=" + self.token)
     # Now we need to grab a session key
-
-test = session()
-test.auth()
-
 
