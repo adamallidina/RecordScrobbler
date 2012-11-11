f836dac9f8b71fe2b5af2d6570f7a776e155c62e
Adam Allidina <adam.allidina@gmail.com>
2012-11-10 22:21:06 -0500

    Added some very basic last.fm functionality

---
+++ lastfm.py
@@ -0,0 +1,38 @@
+###############################################################################
+#
+# LastFM Module for RecordScrobbler
+# // todo description
+#
+###############################################################################
+
+import requests, webbrowser
+
+class session(object):
+
+  """
+  An instance of a session object represents all facilities needed in order for
+  the program to scrobble music to last.fm via their api
+  """
+
+  def __init__(self):
+    self.apiKey    = "021ab74b3f5405138a23c42b8ff1c746"
+    self.predicate = "http://ws.audioscrobbler.com/2.0/?method=auth.gettoken"
+    self.token     = self.gettoken()
+
+  def gettoken(self):
+    url       = self.predicate + "&api_key=" + self.apiKey + "&format=json"
+    response  = requests.get(url)
+    json      = response.json
+    token     = json['token']
+    return token
+
+  def auth(self):
+    predicate = "http://www.last.fm/api/auth/?api_key="
+    # Open up users default web browser to last.fm authentication page
+    webbrowser.open(predicate + self.apiKey + "&token=" + self.token)
+    # Now we need to grab a session key
+
+test = session()
+test.auth()
+
+
