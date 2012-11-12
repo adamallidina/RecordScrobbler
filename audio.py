5c2c4b9f81c4523dbce7fcb9a2b06447247b749d
Adam Allidina <adam.allidina@gmail.com>
2012-11-12 10:57:50 -0500

    Moved main.py to audio.py, transcoding works fine

---
+++ audio.py
@@ -0,0 +1,53 @@
+###############################################################################
+#
+# Main function for Record Scrobbler
+#
+###############################################################################
+
+import lastfm, pydub, wave, pyaudio, time
+
+def grab_audio(filename):
+  """
+  The grab_audio function will use the pyaudio library to record audio from
+  the line in of the computer and will save it in WAVE format
+  Parameters: a string, the name of the file you wish to save the wav file out
+              to
+     Returns: none
+  """
+  # Constants needed
+  CHUNK    = 1024
+  FORMAT   = pyaudio.paInt16
+  CHANNELS = 2
+  RATE     = 44100
+  LENGTH   = 3
+  OUTPUT   = filename
+  record   = pyaudio.PyAudio()
+  stream   = record.open(format=FORMAT, channels=CHANNELS, rate=RATE,
+                         input=True, frames_per_buffer=CHUNK)
+  frames   = []
+  # Recording
+  for i in range(0, int(RATE / CHUNK * LENGTH)):
+    data = stream.read(CHUNK)
+    frames.append(data)
+  # Cleanup
+  stream.stop_stream()
+  stream.close()
+  record.terminate()
+  # Write to file
+  file = wave.open(OUTPUT, 'wb')
+  file.setnchannels(CHANNELS)
+  file.setsampwidth(record.get_sample_size(FORMAT))
+  file.setframerate(RATE)
+  file.writeframes(b''.join(frames))
+  file.close
+
+def export_to_mp3(filename, outfile):
+  """
+  Converts a wave file to mp3, this allows the file to be used with Echo Nest
+  Parameters: a string, the name of the file you wish to convert
+              a string, the name of the output file
+     Returns: none
+  """
+  file = pydub.AudioSegment.from_wav(filename)
+  file.export(outfile, format='mp3')
+
