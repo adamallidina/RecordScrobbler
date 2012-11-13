a3aae2ea285d30b9185c5337e028a368f8959d6a
Adam Allidina <adam.allidina@gmail.com>
2012-11-12 19:52:03 -0500

    Need to start putting everything together

--- main.py
+++ main.py
@@ -4,4 +4,9 @@
 #
 ###############################################################################
 
-import lastfm, audio
+import lastfm, audio, echonest
+
+# FFMPEG Conversion: test.mp3 -ac 1 -ar 22050 -f s16le -t 20 -ss 10 -> test.raw
+
+
+
